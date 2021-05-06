7223d1a2ff055e9d8f42ce1aa5402ee36a4e5209
huzedong <huzedong2008@vip.qq.com>
2021-05-06 17:23:40 +0800

    feat(state): 持久化缓存

--- src/state/src/state.ts
+++ src/state/src/state.ts
@@ -1,4 +1,6 @@
-import { reactive } from "vue";
+import { reactive, watch, toRaw } from "vue";
+
+const STORE_KEY = "STORE";
 
 export interface UserInfo {
    /** 用户名 */
@@ -14,15 +16,41 @@ export interface State {
    token: String | null;
 }
 
-const state:State = ({
-   userInfo: {},
-   token: "",
-});
+/**
+ * 获取存储
+ */
+function getStore() {
+   const defaultOptions: State = {
+      userInfo: {},
+      token: "",
+   };
+
+   const storeStr = localStorage.getItem(STORE_KEY) || "{}";
+   const storeObj = JSON.parse(storeStr) as State;
+
+   Object.assign(defaultOptions, storeObj);
+
+   return defaultOptions;
+}
+
+const state = getStore();
 
 /**
  * 创建state
  * @returns
  */
 export function createState():State {
-   return reactive(state);
+   const stateReactive = reactive(state);
+
+   // 监听
+   watch(stateReactive, (val) => {
+      try {
+         const stroeStr = JSON.stringify(toRaw(val) || {});
+         localStorage.setItem(STORE_KEY, stroeStr);
+      } catch (error) {
+         // error;
+      }
+   });
+
+   return reactive(stateReactive);
 }
